7743b0757b0f2b4ec37a8de0a522c432fa553ba6
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2021-02-24 13:39:29 +0900

    add installation script.

---
+++ script/install_tagslam.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+# TagSLAM installation script
+# requirements: OS: Ubuntu18.04 or later
+# Author: Masahiro Ogawa @Sensyn-robotics.com
+#####
+
+# prepare installation using apt
+sudo apt install python-catkin-tools 
+sudo apt -y remove gtsam
+sudo add-apt-repository -y --remove ppa:bernd-pfrommer/gtsam
+sudo apt-add-repository -y ppa:borglab/gtsam-release-4.0
+sudo apt update
+sudo apt install libgtsam-dev libgtsam-unstable-dev 
+
+# install tagslam root
+ROOTDIR=$(dirname $0)/..
+cd $ROOTDIR
+git submodule update --init --recursive
+catkin config -DCMAKE_BUILD_TYPE=Release
+catkin build
